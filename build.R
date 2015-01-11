cbed4d07379bee39d44ed6687b4f97713e57f51a
Richie Morrisroe <richie.morrisroe@gmail.com>
2015-01-11 18:35:56 +0000

    added build and install loop

--- build.R
+++ build.R
@@ -1,5 +1,7 @@
-create("thesisR")
-document()
+## create("thesisR") not needed no more
 devtools::use_package("psych")
 devtools::use_package(c("ggplot2"))
 devtools::use_package(c("reshape2"))
+document()
+build()
+install()
