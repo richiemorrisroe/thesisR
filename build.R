9232ce2841a1ec96f6ac9c67d910aa012cb4462c
Richie Morrisroe <morrisroe@fb.com>
2015-08-08 18:27:52 +0100

    added GPArotation dependency

--- build.R
+++ build.R
@@ -3,6 +3,7 @@
 devtools::use_package("psych")
 devtools::use_package(c("ggplot2"))
 devtools::use_package(c("reshape2"))
+devtools::use_package(c("GPArotation"))
 devtools::document()
 devtools::build()
 devtools::install()
