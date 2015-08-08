282bc5a17d7604e118705ab633d365f69dd15e4a
Richie Morrisroe <morrisroe@fb.com>
2015-08-08 19:18:07 +0100

    changed functions to be snake case

--- R/misc.R
+++ R/misc.R
@@ -1,8 +1,15 @@
-showTree2 <-  function(e, write=cat) {
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title show_tree2
+##' @param e an R expression
+##' @param write  where to send the results
+##' @return 
+##' @author Richard Morrisroe
+show_tree2 <-  function(e, write=cat) {
     if(!require(codetools)) {
-        stop("Codetools must be installed")
+        stop("Codetools must be installed to use this function")
     }
     quoted <- quote(e)
     showTree(quoted, write=write)
 }
-    
