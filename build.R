05c01871042c6ba76ffbbc8ddd471cdd79a787ed
Richie Morrisroe <morrisroe@fb.com>
2015-08-08 19:42:44 +0100

    added plyr depedency

--- build.R
+++ build.R
@@ -1,6 +1,7 @@
 ## create("thesisR") not needed no more
 
 devtools::use_package("psych")
+devtools::use_package(c("plyr"))
 devtools::use_package(c("ggplot2"))
 devtools::use_package(c("reshape2"))
 devtools::use_package(c("GPArotation"))
