01318b321dbadbb220b2fb6d651834c428e459fb
Richie Morrisroe <morrisroe@fb.com>
2015-08-08 21:32:29 +0100

    added return type for roxygen

--- R/misc.R
+++ R/misc.R
@@ -4,7 +4,7 @@
 ##' @title show_tree2
 ##' @param e an R expression
 ##' @param write  where to send the results
-##' @return 
+##' @return a quoted function object
 ##' @author Richard Morrisroe
 show_tree2 <-  function(e, write=cat) {
     if(!require(codetools)) {
