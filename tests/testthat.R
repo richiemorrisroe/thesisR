c3b2bcc8486adf66af522a6b029b38e413909b39
Richie Morrisroe <morrisroe@fb.com>
2015-08-09 11:14:20 +0100

    added class test for fit_factor_series

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(thesisR)
+
+test_check("thesisR")
---
+++ tests/testthat/test_factor_series.R
@@ -0,0 +1,6 @@
+data(healthoptmind)
+rand <- dat[,grep("RAND", x=names(dat))]
+factors <- 1:12
+rand.fs <- fit_factor_series(rand, factors=factors)
+test_that("class of fs object is factor_series", {
+    expect_equal(class(rand.fs), c("factor_series", "fa"))})
