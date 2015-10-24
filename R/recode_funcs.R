a48eab0231cc413bff4e843bf9c37776f20f39be
morrisroe <morrisroe@fb.com>
2015-10-24 18:43:44 +0100

    lint changes

--- R/recode_funcs.R
+++ R/recode_funcs.R
@@ -5,15 +5,13 @@
 ##' @return a transformed dataframe with the new recodings
 ##' @author Richie Morrisroe
 recode_many <- function (data, vars, Recodings){
-  varlist = list(vars)
+  varlist <- list(vars)
 dataret <- data
   for (i in 1:length(vars)) {
     dataret[,i] <- car::recode(data[,i], recodes=Recodings)
   }
 return(dataret)
 }
-    ##   return(randitems)
-## }
 ##' creates the RAND MOS sum scores
 ##'
 ##' This is unlikely to ever be useful to anyone bar me or potential replicators of my thesis
@@ -22,15 +20,58 @@ return(dataret)
 ##' @return a dataframe containing the sum scores for each variable
 ##' @author Richie Morrisroe
 create_sum_scores <- function(data) {
-data$physfun <- rowMeans(data[,grep("RANDQ[3456789]$|RANDQ[1][012]$",x=names(data))], na.rm=TRUE)
-data$rolelim <- rowMeans(data[,grep("RANDQ[1][3456]$",x=names(data))], na.rm=TRUE)
-data$rolelimem <- rowMeans(data[,grep("RANDQ[1][789]$",x=names(data))], na.rm=TRUE)
-data$energyfat <- rowMeans(data[,grep("RANDQ[2][379]$|RANDQ31$",x=names(data))], na.rm=TRUE)
-data$emwellbeing <- rowMeans(data[,grep("RANDQ[2][4568]$|RANDQ30$",x=names(data))], na.rm=TRUE)
-data$socialfunctioning <- rowMeans(data[,grep("RANDQ20|RANDQ32",x=names(data))], na.rm=TRUE)
-data$pain <- rowMeans(data[,grep("RANDQ[2][12]$",x=names(data))], na.rm=TRUE)
-data$generalhealth <- rowMeans(data[,grep("RANDQ1$|RANDQ[3][3456]$",x=names(data))], na.rm=TRUE)
-data$mindfulness <- rowMeans(data[, grep("MAAS", x=names(data))], na.rm=TRUE)
-data$optimism <- rowMeans(data[,grep("LOTR", x=names(data))], na.rm=TRUE)
+    data$physfun <- rowMeans(
+        data[,
+             grep("RANDQ[3456789]$|RANDQ[1][012]$",x=names(data))],
+        na.rm=TRUE)
+    data$rolelim <- rowMeans(
+        data[,grep(
+            "RANDQ[1][3456]$",
+            x=names(data))],
+        na.rm=TRUE)
+    data$rolelimem <-
+        rowMeans(
+            data[,
+                 grep("RANDQ[1][789]$",
+                      x=names(data))],
+            na.rm=TRUE)
+    data$energyfat <- rowMeans(
+        data[,
+             grep(
+                 "RANDQ[2][379]$|RANDQ31$",
+                 x=names(data))],
+        na.rm=TRUE)
+    data$emwellbeing <- rowMeans(
+        data[,
+             grep(
+                 "RANDQ[2][4568]$|RANDQ30$",
+                 x=names(data))],
+        na.rm=TRUE)
+    data$socialfunctioning <- rowMeans(
+        data[,
+             grep(
+                 "RANDQ20|RANDQ32",
+                 x=names(data))],
+        na.rm=TRUE)
+    data$pain <- rowMeans(
+        data[,
+             grep("RANDQ[2][12]$",
+                   x=names(data))],
+        na.rm=TRUE)
+    data$generalhealth <- rowMeans(
+        data[,
+             grep("RANDQ1$|RANDQ[3][3456]$",
+                  x=names(data))],
+        na.rm=TRUE)
+    data$mindfulness <- rowMeans(
+        data[,
+             grep("MAAS",
+                  x=names(data))],
+        na.rm=TRUE)
+    data$optimism <- rowMeans(
+        data[,
+             grep("LOTR",
+                  x=names(data))],
+        na.rm=TRUE)
 return(data)
 }
