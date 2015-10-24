54c0f75d174e51c171c714a2ed767bc7b240935a
morrisroe <morrisroe@fb.com>
2015-10-24 19:28:14 +0100

    added factor_xtab test

--- tests/testthat/test_factor_series.R
+++ tests/testthat/test_factor_series.R
@@ -1,7 +1,17 @@
 data(healthoptmind)
 rand <- dat[,grep("RAND", x=names(dat))]
-factors <- 1:12
+maas <- dat[,grep("MAASQ", x=names(dat))]
+factors <- 1:8
 rand.fs <- fit_factor_series(rand, factors=factors)
 test_that("class of fs object is factor_series", {
-    expect_equal(class(rand.fs), c("factor_series", "fa"))
+    expect_is(rand.fs, c("factor_series", "fa"))
 })
+maasfact1 <- psych::fa(na.omit(maas), 1,
+                       method="ml",
+                       rotate="oblimin")
+maas1_xtab <- factor_xtab(maasfact1, names=c("Mindfulness"),
+                          label="tab:hom1maas1fact",
+                          caption="Factor Loadings,
+One Factor Solution, MAAS, Sample One")
+test_that("factor_xtab returns an xtable object",
+          expect_is(maas1_xtab, c("data.frame", "xtable")))
