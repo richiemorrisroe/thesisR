f1d91b17c676b2e6540e3f3d028dc9beccc35736
morrisroe <morrisroe@fb.com>
2015-10-25 13:35:02 +0000

    fixed test_that statements to handle multiple asserts

--- tests/testthat/test_scale_functions.R
+++ tests/testthat/test_scale_functions.R
@@ -11,9 +11,10 @@ randrecode1 <- recode_many(randset1,
                            "RANDQ36"),
                            Recodings= (
                                "1=100;2=75;3=50;4=25;5=0"))
-test_that("recode_many returns a df of correct dimensions",
-          expect_is(randrecode1, "data.frame"),
-          expect_equal(dim(randrecode1), dim(randset1)))
+test_that("recode_many returns a df of correct dimensions", {
+          expect_is(randrecode1, "data.frame")
+          expect_equal(dim(randrecode1), dim(randset1))
+      })
 
 sumorder <- c("generalhealth", "physfun", "rolelim",
               "rolelimem", "energyfat",
@@ -22,6 +23,7 @@ sumorder <- c("generalhealth", "physfun", "rolelim",
 totals <- dat[,66:75]
 totals <- totals[,sumorder]
 apatotals <- apa_demo_tables(totals)
-test_that(desc = "apa_demo_tables returns correct data",
-          expect_is(apatotals, "data.frame"),
-          expect_equal(length(apatotals), 5))
+test_that(desc = "apa_demo_tables returns correct data", {
+          expect_is(apatotals, "data.frame")
+          expect_equal(length(apatotals), 5)
+      })
