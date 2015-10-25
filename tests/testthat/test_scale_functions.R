14ef39f8bb4d1e3dba1a7663204d7867620f201f
morrisroe <morrisroe@fb.com>
2015-10-25 15:30:53 +0000

    added all recodings for RAND MOS

--- tests/testthat/test_scale_functions.R
+++ tests/testthat/test_scale_functions.R
@@ -11,6 +11,54 @@ randrecode1 <- recode_many(randset1,
                            "RANDQ36"),
                            Recodings= (
                                "1=100;2=75;3=50;4=25;5=0"))
+randrecode2 <- recode_many(randset2,
+                          vars=c("RANDQ3",
+                          "RANDQ4",
+                          "RANDQ5",
+                          "RANDQ6",
+                          "RANDQ7",
+                          "RANDQ8",
+                          "RANDQ9",
+                          "RANDQ10",
+                          "RANDQ11",
+                          "RANDQ12"),
+                          Recodings="1=0;2=50;3=100")
+randset3 <- paste(rand, c(13:19), sep="")
+randset3 <- hom[,randset3]
+randrecode3 <- recode_many(randset3,
+                          vars=c("RANDQ13",
+                          "RANDQ14",
+                          "RANDQ15",
+                          "RANDQ16",
+                          "RANDQ17",
+                          "RANDQ18",
+                          "RANDQ19"),
+                          Recodings="1=0;2=100")
+randset4 <- paste(rand, c(21,23,26,27,30), sep="")
+randset4 <- hom[,randset4]
+randrecode4 <- recode_many(randset4,
+                          vars=c("RANDQ21",
+                          "RANDQ23",
+                          "RANDQ26",
+                          "RANDQ27",
+                          "RANDQ30"),
+                          Recodings="1=100;2=80;3=60;4=40;5=20;6=0")
+randset5 <- paste(rand, c(24,25,28,29,31), sep="")
+randset5 <- hom[,randset5]
+randrecode5 <- recode_many(randset5,
+                          vars=c("RANDQ24",
+                          "RANDQ25",
+                          "RANDQ28",
+                          "RANDQ29",
+                          "RANDQ31"),
+                          Recodings="1=0;2=20;3=40;4=60;5=80;6=100")
+randset6 <- paste(rand, c(32,33,35), sep="")
+randset6 <- hom[,randset6]
+randrecode6 <- RecodeMany(randset6,
+                          vars=c("RANDQ32",
+                          "RANDQ33",
+                          "RANDQ35"),
+                          Recodings="1=0;2=25;3=50;4=75;5=100")
 test_that("recode_many returns a df of correct dimensions", {
           expect_is(randrecode1, "data.frame")
           expect_equal(dim(randrecode1), dim(randset1))
