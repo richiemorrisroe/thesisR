fddc4e426016934bc6463f70714954255ca63aa9
morrisroe <morrisroe@fb.com>
2015-10-26 13:58:41 +0000

    added nonfunctional test for create_combinations

--- tests/testthat/test_cross_validation.R
+++ tests/testthat/test_cross_validation.R
@@ -1,5 +1,16 @@
+data(healthoptmind)
 multi_folds <- 10
 opt.part <- caret::createMultiFolds(na.omit(dat$optimism), k=multi_folds)
 optfolds <- train_test_sets(opt.part, dat)
 test_that("the correct number of folds are created",
           expect_equal(length(optfolds), multi_folds * 10))
+maas <- dat[,grep("MAASQ", x=names(dat))]
+test_splits <- 4
+big_splits <- 10
+split_maas <- split_sample(maas, split=test_splits)
+maas_combinations <- create_combinations(split_maas)
+split_maas10 <- split_sample(maas, split=big_splits)
+## test_that("create combinations returns the right dim train and test sets:",{
+##           expect_gt(lapply(split_maas["train"], dim),
+##                     lapply(split_maas, function (x) "[[", "test", dim))
+##       })
